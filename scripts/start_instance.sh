439602fc03977e2e960963262346003b62ba844c
ktak2001 <kazuki.takehi@gmail.com>
2024-07-12 12:27:10 +0900

    update

--- scripts/start_instance.sh
+++ scripts/start_instance.sh
@@ -9,4 +9,4 @@ gcloud compute instances add-metadata set-youtuber-superchats \
 --metadata-from-file startup-script=/Users/takehikazuki/Desktop/my_app3/ranking_app/scripts/startup-script.sh \
 --zone=asia-northeast1-a
 
-gcloud compute instances reset set-youtuber-superchats --zone=asia-northeast1-a
+gcloud compute instances start set-youtuber-superchats --zone=asia-northeast1-a
